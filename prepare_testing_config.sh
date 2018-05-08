b7f56b00947637bb0853faf0f9b677a1116e5299
Alexander Mentyu <notuxius@gmail.com>
2018-05-09 00:41:04 +0300

    Initial commit of shell script for preparing testing configuration

---
+++ prepare_testing_config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# TODO add info dialogs upon operations - copying, modifying, etc.
+
+target_conf_dir=~/.config/manjaro-tools/manjaro-tools.conf
+
+# check if user's home config file already exists - and displays info and confirmation for overwriting it -
+# then copy system-wide manjaro tools config file into user's home directory -
+# for further modifications - user's home config file take precedence over the /etc system's one
+if [ -f $(eval echo $target_conf_dir) ]; then
+    echo 'you have existing manjaro tools configuration in your home directory'
+    cp -i /etc/manjaro-tools/manjaro-tools.conf $target_conf_dir
+else
+    cp /etc/manjaro-tools/manjaro-tools.conf $target_conf_dir0
+fi
+
+# use unstable repos
+sed -i 's/# target_branch=stable/target_branch=unstable/' $target_conf_dir
+
+# change iso label from Manjaro into Netrunner
+sed -i 's/# dist_branding="MJRO"/dist_branding="NTRR"/' $target_conf_dir
+
