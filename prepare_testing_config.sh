fa71b503bf9724b471938a8d208ce9538b495b13
Alexander Mentyu <notuxius@gmail.com>
2018-05-09 00:44:54 +0300

    Update and fix typo in preparing testing configuration script

--- prepare_testing_config.sh
+++ prepare_testing_config.sh
@@ -2,21 +2,21 @@
 
 # TODO add info dialogs upon operations - copying, modifying, etc.
 
-target_conf_dir=~/.config/manjaro-tools/manjaro-tools.conf
+target_conf_file=~/.config/manjaro-tools/manjaro-tools.conf
 
 # check if user's home config file already exists - and displays info and confirmation for overwriting it -
-# then copy system-wide manjaro tools config file into user's home directory -
-# for further modifications - user's home config file take precedence over the /etc system's one
-if [ -f $(eval echo $target_conf_dir) ]; then
+# then copy system-wide manjaro tools config file into user's home directory for further modifications - 
+# user's home config file take precedence over the /etc system's one
+if [ -f $(eval echo $target_conf_file) ]; then
     echo 'you have existing manjaro tools configuration in your home directory'
-    cp -i /etc/manjaro-tools/manjaro-tools.conf $target_conf_dir
+    cp -i /etc/manjaro-tools/manjaro-tools.conf $target_conf_file
 else
-    cp /etc/manjaro-tools/manjaro-tools.conf $target_conf_dir0
+    cp /etc/manjaro-tools/manjaro-tools.conf $target_conf_file
 fi
 
 # use unstable repos
-sed -i 's/# target_branch=stable/target_branch=unstable/' $target_conf_dir
+sed -i 's/# target_branch=stable/target_branch=unstable/' $target_conf_file
 
 # change iso label from Manjaro into Netrunner
-sed -i 's/# dist_branding="MJRO"/dist_branding="NTRR"/' $target_conf_dir
+sed -i 's/# dist_branding="MJRO"/dist_branding="NTRR"/' $target_conf_file
 
