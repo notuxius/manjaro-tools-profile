bb72bd502799a661fb80c6636bb40721ad80f503
Alexander Mentyu <notuxius@gmail.com>
2018-05-09 14:40:11 +0300

    Add config file existence check before executing script

--- prepare_testing_config.sh
+++ prepare_testing_config.sh
@@ -2,16 +2,23 @@
 
 # TODO add info dialogs upon operations - copying, modifying, etc.
 
-target_conf_file=~/.config/manjaro-tools/manjaro-tools.conf
+
+if [ -f $(eval echo /etc/manjaro-tools/manjaro-tools.conf) ]; then
+    source_conf_file=/etc/manjaro-tools/manjaro-tools.conf
+    target_conf_file=~/.config/manjaro-tools/manjaro-tools.conf
+else
+    echo 'System manjaro tools configuration not found'
+    exit 1
+fi
 
 # check if user's home config file already exists - and displays info and confirmation for overwriting it -
 # then copy system-wide manjaro tools config file into user's home directory for further modifications - 
 # user's home config file take precedence over the /etc system's one
 if [ -f $(eval echo $target_conf_file) ]; then
     echo 'you have existing manjaro tools configuration in your home directory'
-    cp -i /etc/manjaro-tools/manjaro-tools.conf $target_conf_file
+    cp -i $source_conf_file $target_conf_file
 else
-    cp /etc/manjaro-tools/manjaro-tools.conf $target_conf_file
+    cp $source_conf_file $target_conf_file
 fi
 
 # use unstable repos
